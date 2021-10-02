70c6b0d55a451fe238b3c14f7252e983854460ba
ALSHIHAB <43612338+ALSHIHAB@users.noreply.github.com>
2021-10-03 02:29:38 +0800

    Fix full-stack-challenge-code by using dependency injection concept

--- CacheManager.php
+++ CacheManager.php
@@ -1,31 +1,98 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: isnain
- * Date: 09.08.21
- * Time: 10:14
+ * User: Najib
+ * Date: 03.10.21
+ * Time: 02:17
  */
+interface CacheSystemInterface 
+{
+    public function connect(string $host, string $port);
+    public function set(string $key, string $value, string $is_compressed=null, string $ttl=null);
+    public function get(string $key);
+}
+
+class MemcacheSystem implements CacheSystemInterface
+{
+
+    protected $memcache;
+
+    public function __construct()
+    {
+        // can be injected to the constructor if DIC is implemented in the app
+        $this->memcache = new \Memcache();
+    }
+
+    // with Dependency Injection Container 
+    // public function __construct(\Memcache $memcache)
+    // {
+    //     // can be injected to the constructor if DIC is implemented in the app
+    //     $this->memcache = $memcache;
+    // }
+
+    public function connect(string $host, string $port) { 
+        $this->memcache->connect($host,$port);
+    }
+
+    public function set(string $key, string $value, ?string $is_compressed = null, ?string $ttl = null) { 
+        $this->memcache->set($key,$value,$is_compressed,$ttl);
+    }
+
+    public function get(string $key) { 
+        return $this->memcache->get($key);
+    }
+    
+}
+
+class RedisSystem implements CacheSystemInterface
+{
+
+    protected $redis;
+
+    public function __construct()
+    {
+        // can be injected to the constructor if DIC is implemented in the app
+        $this->redis = new \Redis();
+    }
+
+    // with Dependency Injection Container 
+    // public function __construct(\Redis $redis)
+    // {
+    //     // can be injected to the constructor if DIC is implemented in the app
+    //     $this->redis = $redis;
+    // }
+
+    public function connect(string $host, string $port) { 
+        $this->redis->connect($host,$port);
+    }
+
+    public function set(string $key, string $value, ?string $is_compressed = null, ?string $ttl = null) { 
+        $this->redis->set($key,$value,$ttl);
+    }
+
+    public function get(string $key) { 
+        return $this->redis->get($key);
+    }
+
+    public function lpush(string $key, string $value) {
+        $this->redis->lPush($key,$value);
+    }
+    
+}
 
 class CacheManager
 {
     private $cache;
+    private $cacheSystem;
 
-    public function setCache(string $cachingSystem)
+    public function __construct(CacheSystemInterface $cacheSystem)
     {
+        $this->cache = $cacheSystem;
+    }
 
-        switch ($cachingSystem){
-
-            case "redis":
-                $this->cache=new \Redis();
-                break;
-            case "memcache":
-                $this->cache=new \Memcache();
-                break;
-            default:
-                throw new \Exception("Cache Manager Not Found");
-
-        }
-
+    public function setCache(CacheSystemInterface $cacheSystem)
+    {
+        $this->cache = $cacheSystem;
     }
 
     public function connect(string $host, string $port){
@@ -34,44 +101,34 @@ class CacheManager
 
     }
 
-    public function set(string $key, string $value, string $is_compressed=null, string $ttl=null){
-
-        if($this->cache instanceof \Memcache)
+    public function set(string $key, string $value, string $is_compressed=null, string $ttl=null)
+    {
             $this->cache->set($key,$value,$is_compressed,$ttl);
-        else if($this->cache instanceof \Redis)
-            $this->cache->set($key,$value,$ttl);
     }
 
     public function get(string $key){
-
         return $this->cache->get($key);
     }
 
     public function lpush(string $key, string $value){
+            if(! $this->cache instanceof RedisSystem) die("Cannot use lpush for this cache system");
 
-        if($this->cache instanceof \Memcache)
-            throw new \Exception("method not supported");
-        else if($this->cache instanceof \Redis)
-            $this->cache->lPush($key,$value);
-
+            $this->cache->lpush($key,$value);
     }
-
 
 }
 
-$cm=new CacheManager();
 
-$cm->setCache('redis');
+$cm=new CacheManager(new RedisSystem);
+
 $cm->connect('somehost','121');
 $cm->set('one','1');
 $cm->lpush('two','1');
 $cm->lpush('two','2');
 echo $cm->get('one');
 
-$cm->setCache('memcache');
+$cm->setCache(new MemcacheSystem);
 $cm->connect('somehost','121');
 $cm->set('one','1');
 $cm->lpush('two','2'); // generates exception
 echo $cm->get('one');
-
-
