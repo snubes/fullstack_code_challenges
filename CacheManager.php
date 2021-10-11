824bf28470658d4bcc0832322b89ca59fc65dfed
katlassi <katlassi@coyote-group.com>
2021-10-11 09:01:52 +0200

    Refactoring

--- CacheManager.php
+++ CacheManager.php
@@ -1,77 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: isnain
- * Date: 09.08.21
- * Time: 10:14
- */
 
+namespace Snubes;
+
+/**
+ * The Factory for the available cache systems
+ */
 class CacheManager
 {
-    private $cache;
-
-    public function setCache(string $cachingSystem)
+    /**
+     * A factory method for constructiog a cache object
+     * @param Config $config
+     * @return mixed|void
+     */
+    public static function connect(Config $config)
     {
-
-        switch ($cachingSystem){
-
-            case "redis":
-                $this->cache=new \Redis();
-                break;
-            case "memcache":
-                $this->cache=new \Memcache();
-                break;
-            default:
-                throw new \Exception("Cache Manager Not Found");
-
+        $className = $config->driver . 'Cache';
+        if (class_exists($className)) {
+            $cacheSystem = new $className();
+            $cacheSystem->connect($config->host, $config->port);
+            return $cacheSystem;
         }
-
     }
-
-    public function connect(string $host, string $port){
-
-        $this->cache->connect($host,$port);
-
-    }
-
-    public function set(string $key, string $value, string $is_compressed=null, string $ttl=null){
-
-        if($this->cache instanceof \Memcache)
-            $this->cache->set($key,$value,$is_compressed,$ttl);
-        else if($this->cache instanceof \Redis)
-            $this->cache->set($key,$value,$ttl);
-    }
-
-    public function get(string $key){
-
-        return $this->cache->get($key);
-    }
-
-    public function lpush(string $key, string $value){
-
-        if($this->cache instanceof \Memcache)
-            throw new \Exception("method not supported");
-        else if($this->cache instanceof \Redis)
-            $this->cache->lPush($key,$value);
-
-    }
-
-
 }
-
-$cm=new CacheManager();
-
-$cm->setCache('redis');
-$cm->connect('somehost','121');
-$cm->set('one','1');
-$cm->lpush('two','1');
-$cm->lpush('two','2');
-echo $cm->get('one');
-
-$cm->setCache('memcache');
-$cm->connect('somehost','121');
-$cm->set('one','1');
-$cm->lpush('two','2'); // generates exception
-echo $cm->get('one');
 
 
