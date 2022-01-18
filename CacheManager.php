47e86c8fb55cb5e4261a22d92603668c57abdf89
Dragan <dragan.begovski@karpa.mk>
2022-01-18 16:09:06 +0100

    dragan solution

--- CacheManager.php
+++ CacheManager.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: isnain
- * Date: 09.08.21
- * Time: 10:14
+ * User: dragan
+ * Date: 18.01.22
+ * Time: 10:30
  */
 
 class CacheManager
@@ -12,14 +11,13 @@ class CacheManager
 
     public function setCache(string $cachingSystem)
     {
-
-        switch ($cachingSystem){
+        switch ($cachingSystem) {
 
             case "redis":
-                $this->cache=new \Redis();
+                $this->cache = new \Redis();
                 break;
             case "memcache":
-                $this->cache=new \Memcache();
+                $this->cache = new \Memcache();
                 break;
             default:
                 throw new \Exception("Cache Manager Not Found");
@@ -30,48 +28,68 @@ class CacheManager
 
     public function connect(string $host, string $port){
 
-        $this->cache->connect($host,$port);
+        try {
+            $this->cache->connect($host,$port);
+        } catch (Exception $e) {
+            echo ( "Cannot connect to cache server:".$e->getMessage() );
+        }      
 
     }
 
-    public function set(string $key, string $value, string $is_compressed=null, string $ttl=null){
+    public function set(string $key, string $value, $is_compressed=null, $ttl = null): bool{
 
-        if($this->cache instanceof \Memcache)
-            $this->cache->set($key,$value,$is_compressed,$ttl);
-        else if($this->cache instanceof \Redis)
-            $this->cache->set($key,$value,$ttl);
+        if($this->cache instanceof \Memcache && isset($key)) {
+                $this->cache->set($key,$value,$is_compressed,$ttl);
+            return true;
+        } else if($this->cache instanceof \Redis && isset($key)) {        
+                $this->cache->set($key,$value,$ttl);
+            return true;
+        } else {         
+                throw new \InvalidArgumentException("Key doesn't exist");
+            return false;
+        }
+
     }
 
-    public function get(string $key){
+    public function get(string $key): bool{
 
-        return $this->cache->get($key);
+        if (isset($key)) {
+                return $this->cache->get($key);
+            return true;
+        } else  {            
+                throw new \Exception("Key doesn't exist");
+            return false;
+        }
+
     }
 
     public function lpush(string $key, string $value){
 
-        if($this->cache instanceof \Memcache)
-            throw new \Exception("method not supported");
-        else if($this->cache instanceof \Redis)
+        if($this->cache instanceof \Memcache || $this->cache instanceof \Redis) {
             $this->cache->lPush($key,$value);
+        }
+        else {
+            throw new Exception("method not supported");
+        }
 
     }
-
-
 }
 
-$cm=new CacheManager();
+$cm = new CacheManager();
 
 $cm->setCache('redis');
-$cm->connect('somehost','121');
+$cm->connect('localhost','6379');
 $cm->set('one','1');
 $cm->lpush('two','1');
 $cm->lpush('two','2');
 echo $cm->get('one');
 
 $cm->setCache('memcache');
-$cm->connect('somehost','121');
+$cm->connect('localhost','11211');
 $cm->set('one','1');
-$cm->lpush('two','2'); // generates exception
+try{
+    $cm->lpush('two','2');
+} catch (Exception $e) {
+    echo 'Message: ' .$e->getMessage();
+}
 echo $cm->get('one');
-
-
