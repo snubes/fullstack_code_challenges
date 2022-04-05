e61a448673d523e125c9a43fb66fc9055e265683
Ahmad Azizi <ahmad.azizi.dev@gmail.com>
2022-04-05 16:44:49 +0300

    Refactor Cache Manager test based on DRY

--- src/Cache/CacheContainer.php
+++ src/Cache/CacheContainer.php
@@ -5,12 +5,11 @@ namespace App\Cache;
 /**
  *  implementation singleton pattern
  */
-
 final class CacheContainer
 {
     private static array $instances = [];
 
-    public static function getInstance(string $className, $host, $port)
+    public static function getInstance(string $className, $host, $port): CacheServiceInterface
     {
         if (!isset(self::$instances[$className])) {
             self::$instances[$className] = new $className($host, $port);
--- tests/Integration/CacheManagerTest.php
+++ tests/Integration/CacheManagerTest.php
@@ -15,11 +15,21 @@ class CacheManagerTest extends TestCase
         $this->cacheManager->flushAll();
     }
 
+    public function cacheTypeDataProvider(): array
+    {
+        return [
+            ['redis'],
+            ['memcached']
+        ];
+    }
+
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_set_and_get()
+    public function set_and_get($cacheType)
     {
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->cacheManager = CacheManager::setCache();
         $this->cacheManager->set('one', '1');
         $this->assertEquals(1, $this->cacheManager->get('one'));
@@ -28,58 +38,64 @@ class CacheManagerTest extends TestCase
 
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_get_not_exist_key()
+    public function get_not_exist_key($cacheType)
     {
-        $this->cacheManager = CacheManager::setCache();
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->assertFalse($this->cacheManager->get('one'));
     }
 
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_LPop_not_exist_key()
+    public function lPop_not_exist_key($cacheType)
     {
-        $this->cacheManager = CacheManager::setCache();
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->assertFalse($this->cacheManager->lPop('one'));
     }
 
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_lPop_not_work_on_not_list_data()
+    public function lPop_not_work_on_not_list_data($cacheType)
     {
-        $this->cacheManager = CacheManager::setCache();
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->cacheManager->set('one', '1');
         $this->assertFalse($this->cacheManager->lPop('one'));
     }
 
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_lPush_not_work_on_not_list_data()
+    public function lPush_not_work_on_not_list_data($cacheType)
     {
-        $this->cacheManager = CacheManager::setCache();
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->cacheManager->set('one', '1');
         $this->assertFalse($this->cacheManager->lPush('one'));
     }
 
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_lPush_and_lPop()
+    public function lPush_and_lPop($cacheType)
     {
-        $this->cacheManager = CacheManager::setCache();
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->cacheManager->lPush('one', '1');
         $this->assertEquals(1, $this->cacheManager->lPop('one'));
     }
 
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_lPush_append_and_lPop()
+    public function lPush_append_and_lPop($cacheType)
     {
-        $this->cacheManager = CacheManager::setCache();
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->cacheManager->lPush('one', '1');
         $this->cacheManager->lPush('one', '2');
         $this->assertEquals(2, $this->cacheManager->lPop('one'));
@@ -90,10 +106,11 @@ class CacheManagerTest extends TestCase
 
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_LPush_can_append_multiple_values()
+    public function lPush_can_append_multiple_values($cacheType)
     {
-        $this->cacheManager = CacheManager::setCache('redis');
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->cacheManager->lPush('one', '1', '2', '3');
         $this->cacheManager->lPush('one', '4', '5', '6');
         $this->assertEquals(6, $this->cacheManager->lPop('one'));
@@ -106,114 +123,16 @@ class CacheManagerTest extends TestCase
 
     /**
      * @test
+     * @dataProvider cacheTypeDataProvider
      */
-    public function redis_set_over_write()
+    public function set_over_write($cacheType)
     {
-        $this->cacheManager = CacheManager::setCache();
+        $this->cacheManager = CacheManager::setCache($cacheType);
         $this->cacheManager->set('one', '1');
         $this->cacheManager->set('one', '2');
         $this->assertEquals(2, $this->cacheManager->get('one'));
     }
 
-    /**
-     * @test
-     */
-    public function memcached_set_and_get()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->cacheManager->set('one', '1');
-        $this->assertEquals(1, $this->cacheManager->get('one'));
-        $this->assertEquals(1, $this->cacheManager->get('one'), 'double call does not have same result on get method');
-    }
-
-    /**
-     * @test
-     */
-    public function memcached_get_when_not_exist_key()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->assertFalse($this->cacheManager->get('one'));
-    }
-
-    /**
-     * @test
-     */
-    public function memcached_lPop_not_exist_key()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->assertFalse($this->cacheManager->lPop('one'));
-    }
-
-    /**
-     * @test
-     */
-    public function memcached_lPop_not_work_on_not_list_data()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->cacheManager->set('one', '1');
-        $this->assertFalse($this->cacheManager->lPop('one'));
-    }
-
-    /**
-     * @test
-     */
-    public function memcached_lPush_not_work_on_not_list_data()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->cacheManager->set('one', '1');
-        $this->assertFalse($this->cacheManager->lPush('one'));
-    }
-
-    /**
-     * @test
-     */
-    public function memcached_lPush_and_lPop()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->cacheManager->lPush('one', '1');
-        $this->assertEquals(1, $this->cacheManager->lPop('one'));
-    }
-
-    /**
-     * @test
-     */
-    public function memcached_lPush_append_and_lPop()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->cacheManager->lPush('one', '1');
-        $this->cacheManager->lPush('one', '2');
-        $this->assertEquals(2, $this->cacheManager->lPop('one'));
-        $this->assertEquals(1, $this->cacheManager->lPop('one'));
-        $this->assertFalse($this->cacheManager->lPop('one'));
-        $this->assertFalse($this->cacheManager->get('one'));
-    }
-
-    /**
-     * @test
-     */
-    public function memcached_LPush_can_append_multiple_values()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->cacheManager->lPush('one', '1', '2', '3');
-        $this->cacheManager->lPush('one', '4', '5', '6');
-        $this->assertEquals(6, $this->cacheManager->lPop('one'));
-        $this->assertEquals(5, $this->cacheManager->lPop('one'));
-        $this->assertEquals(4, $this->cacheManager->lPop('one'));
-        $this->assertEquals(3, $this->cacheManager->lPop('one'));
-        $this->assertEquals(2, $this->cacheManager->lPop('one'));
-        $this->assertEquals(1, $this->cacheManager->lPop('one'));
-    }
-
-    /**
-     * @test
-     */
-    public function memcached_set_over_write()
-    {
-        $this->cacheManager = CacheManager::setCache('memcached');
-        $this->cacheManager->set('one', '1');
-        $this->cacheManager->set('one', '2');
-        $this->assertEquals(2, $this->cacheManager->get('one'));
-    }
 
     /**
      * @test
