6763c547be26fd2662c567fb663413ba98f65e62
isnain31 <isnbit@yahoo.com>
2022-05-23 14:14:43 +0200

    cachemanager efe

--- CacheManager.php
+++ CacheManager.php
@@ -1,77 +1,127 @@
 <?php
+
+namespace Cache;
+
+class MemCacheSystem implements CacheSystemInterface
+{
+
+    private $system;
+
+    public function __construct()
+    {
+        $this->system = new \Memcache();
+    }
+
+    public function set($key, $value, $isCompressed, $ttl)
+    {
+        $this->system->set($key, $value, $isCompressed, $ttl);
+    }
+
+    public function connect($host, $port)
+    {
+        $this->system->connect($host, $port);
+    }
+
+    public function get($key)
+    {
+        return $this->system->get($key);
+    }
+
+    public function lpush($key, $value)
+    {
+        throw new \Exception('Method not supported');
+    }
+}
+
+interface CacheSystemInterface
+{
+    public function connect($host, $port);
+
+    public function get($key);
+
+    public function lpush($key, $value);
+}
+
+class RedisCacheSystem implements CacheSystemInterface
+{
+
+    private $system;
+
+    public function __construct()
+    {
+        $this->system = new \Redis();
+    }
+
+    public function connect($host, $port)
+    {
+        $this->system->connect($host, $port);
+    }
+
+    public function set($key, $value, $ttl = 3000)
+    {
+        $this->system->set($key, $value, $ttl);
+    }
+
+    public function get($key)
+    {
+        return $this->system->get($key);
+    }
+
+    public function lpush($key, $value)
+    {
+        $this->system->lpush($key, $value);
+    }
+}
+
 /**
  * Created by PhpStorm.
  * User: isnain
  * Date: 09.08.21
  * Time: 10:14
  */
-
 class CacheManager
 {
-    private $cache;
+    private CacheSystemInterface $cache;
 
-    public function setCache(string $cachingSystem)
+    public function setCache(CacheSystemInterface $cachingSystem)
     {
-
-        switch ($cachingSystem){
-
-            case "redis":
-                $this->cache=new \Redis();
-                break;
-            case "memcache":
-                $this->cache=new \Memcache();
-                break;
-            default:
-                throw new \Exception("Cache Manager Not Found");
-
-        }
-
+        $this->cache = $cachingSystem;
     }
 
-    public function connect(string $host, string $port){
-
-        $this->cache->connect($host,$port);
-
+    public function getCache(): CacheSystemInterface
+    {
+        return $this->cache;
     }
 
-    public function set(string $key, string $value, string $is_compressed=null, string $ttl=null){
-
-        if($this->cache instanceof \Memcache)
-            $this->cache->set($key,$value,$is_compressed,$ttl);
-        else if($this->cache instanceof \Redis)
-            $this->cache->set($key,$value,$ttl);
+    public function connect(string $host, string $port)
+    {
+        $this->cache->connect($host, $port);
     }
 
-    public function get(string $key){
-
+    public function get(string $key)
+    {
         return $this->cache->get($key);
     }
 
-    public function lpush(string $key, string $value){
-
-        if($this->cache instanceof \Memcache)
-            throw new \Exception("method not supported");
-        else if($this->cache instanceof \Redis)
-            $this->cache->lPush($key,$value);
-
+    public function lpush(string $key, string $value)
+    {
+        $this->cache->lPush($key, $value);
     }
-
 
 }
 
-$cm=new CacheManager();
+$cm = new CacheManager();
 
-$cm->setCache('redis');
-$cm->connect('somehost','121');
-$cm->set('one','1');
-$cm->lpush('two','1');
-$cm->lpush('two','2');
+$cm->setCache(new RedisCacheSystem());
+$cm->connect('somehost', '121');
+$cm->getCache()->set('one', '1');
+$cm->lpush('two', '1');
+$cm->lpush('two', '2');
 echo $cm->get('one');
 
-$cm->setCache('memcache');
-$cm->connect('somehost','121');
-$cm->set('one','1');
-$cm->lpush('two','2'); // generates exception
+$cm->setCache(new MemCacheSystem());
+$cm->connect('somehost', '121');
+$cm->getCache()->set('one', '1', 1,'5000');
+$cm->lpush('two', '2'); // generates exception
 echo $cm->get('one');
-
 
